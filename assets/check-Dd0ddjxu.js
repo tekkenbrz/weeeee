7a3624e0d4c9a145f71947238060958a7950eb5b
tekkenbrz <naimekirato@gmail.com>
2026-03-13 23:46:13 -0300

    nice update

---
+++ assets/check-Dd0ddjxu.js
@@ -0,0 +1,1 @@
+import{c}from"./index-h-Z7Tpsg.js";const e=c("Check",[["path",{d:"M20 6 9 17l-5-5",key:"1gmf2c"}]]);export{e as C};
---
+++ assets/zap-C15ldhi-.js
@@ -0,0 +1,1 @@
+import{c as o}from"./index-h-Z7Tpsg.js";const s=o("Zap",[["polygon",{points:"13 2 3 14 12 14 11 22 21 10 12 10 13 2",key:"45s27k"}]]);export{s as Z};
